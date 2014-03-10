0c4619415aba61f47e9a0f579b4dbacfefd0557d
linuxmaier <andrew.eric.maier@gmail.com>
2014-03-09 20:04:56 -0500

    changing path resolution

--- db_connect.py
+++ db_connect.py
@@ -4,8 +4,9 @@ from sqlalchemy import create_engine
 from os import path
 
 def db_connect(db_name, host):
+	cnf_path = os.path.dirname(__file__).join('my.cnf')
 	'''Returns a sqlalchemy database engine based on db_name on host. It uses a my.cnf file in this folder to supply user info.'''
-	myDB = URL(drivername='mysql', host='localhost', database=db_name, query={ 'read_default_file' : path.abspath('my.cnf') })
+	myDB = URL(drivername='mysql', host='localhost', database=db_name, query={ 'read_default_file' : cnf_path })
 	engine = create_engine(name_or_url=myDB, echo=False)
 	engine.connect() #to test the db connection
 	return engine
